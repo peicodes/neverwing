ff03036214c166de5b7b676ca1678ed350f1fd6f
Pei Li <peihongli94@gmail.com>
2016-12-12 23:38:22 -0500

    fix play_game.rb

--- bin/play_game.rb
+++ bin/play_game.rb
@@ -1,4 +1,4 @@
 require_relative '../neverwing'
 
 LOGIN_URL = 'the login url from the network log'.freeze
-Neverwing.new(LOGIN_URL).play(score: 1000, experience: 1000)
+Neverwing.new(LOGIN_URL).play(coins: 1000, experience: 1000)
